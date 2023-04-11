5a404372972ce72dcfc338733b49fb8739254fd4
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-11 16:13:14 +0200

    added dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import { ThemeContext } from "./contexts/ThemeContext";
 
 function App() {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+      setTheme('dark');
+    }
+    else {
+      setTheme('light');
+    }
+  }, [])
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
   return (
-    <Routes>
-      <Route path="/" element={<Home />}/>
-    </Routes>
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </ThemeContext.Provider>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { ThemeContext } from '../contexts/ThemeContext'
+import { MdLightMode, MdDarkMode } from 'react-icons/md'
 
 export default function Header() {
 
+  const {theme, setTheme} = useContext(ThemeContext)
+
   return (
-    <header className="fixed w-full top-0 left-0 bg-gray">
+    <header className="fixed w-full top-0 left-0 dark:bg-gray bg-white">
         <nav>
             <div className='max-w-[1300px] py-7 flex justify-between items-center w-auto mx-auto'>
               <Link to={'/'}>
-                  <h2 className='text-2xl font-bold leading-[29px]'>LoL Stats</h2>
+                  <h2 className='text-2xl text-black dark:text-white font-bold leading-[29px]'>LoL Stats</h2>
               </Link>
 
-              <div className='navlinks transition ease-linear duration-100 flex group justify-between items-center w-[450px]'>
-                  <Link className='transition ease-in-out' to={'/'}>Home</Link>
-                  <Link className='transition ease-in-out' to={'/leaderboard'}>Leaderboard</Link>
-                  <Link className='transition ease-in-out' to={'/free-rotation'}>Free rotation</Link>
-                  <Link className='transition ease-in-out' to={'/about'}>About</Link>
+              <div className='w-[520px] transition ease-linear duration-100 flex group justify-between items-center'>
+                  <div className='navlinks w-[450px] hover:text-[#1d1d1d80] dark:hover:text-[#FFFFFF80] flex text-black dark:text-white justify-between items-center'>
+                    <Link className='transition ease-in-out hover:text-black dark:hover:text-white' to={'/'}>Home</Link>
+                    <Link className='transition ease-in-out hover:text-black dark:hover:text-white' to={'/leaderboard'}>Leaderboard</Link>
+                    <Link className='transition ease-in-out hover:text-black dark:hover:text-white' to={'/free-rotation'}>Free rotation</Link>
+                    <Link className='transition ease-in-out hover:text-black dark:hover:text-white' to={'/about'}>About</Link>
+                  </div>
+                  <button onClick={() => {setTheme(theme === "dark" ? "light" : "dark")}}>
+                    {theme === 'light' ? 
+                      <MdDarkMode className='w-7 h-7'/> : 
+                      <MdLightMode className='fill-white w-7 h-7'/>
+                    }
+                  </button>
               </div>
             </div>
         </nav>
