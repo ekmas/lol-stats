55e6dc9b56d0c1ccba1b88913804eb7d870270d0
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-12 09:40:04 +0200

    added shadow on scroll

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,17 @@ import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { ThemeContext } from '../contexts/ThemeContext'
 import { MdLightMode, MdDarkMode } from 'react-icons/md'
+import { useScrollPosition } from '../hooks/useScrollPosition'
 
 export default function Header() {
 
   const {theme, setTheme} = useContext(ThemeContext)
 
+  const scrollPosition = useScrollPosition()
+
   return (
     <header className="fixed w-full top-0 left-0 dark:bg-gray bg-white">
-        <nav>
+        <nav className={scrollPosition > 0 ? 'shadow transition-shadow shadow-[#1d1d1d48] dark:shadow-[#ffffff]' : 'shadow-none'}>
             <div className='max-w-[1300px] py-7 flex justify-between items-center w-auto mx-auto'>
               <Link to={'/'}>
                   <h2 className='text-2xl text-black dark:text-white font-bold leading-[29px]'>LoL Stats</h2>
