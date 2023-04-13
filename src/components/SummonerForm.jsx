c6f95be7f42b5db32284d543281830ddca2ceea6
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-13 17:55:57 +0200

    added ErrorModal and form validation

--- src/components/SummonerForm.jsx
+++ src/components/SummonerForm.jsx
@@ -1,11 +1,30 @@
 import React, { useState } from 'react'
 import { MdExpandMore, MdSearch } from 'react-icons/md'
 import RegionModal from './RegionModal'
+import ErrorModal from './ErrorModal'
 
 export default function SummonerForm() {
 
   const [isModalActive, setIsModalActive] = useState(false)
+  const [username, setUsername] = useState('')
   const [region, setRegion] = useState(null)
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(username.trim() !== '' && region !== null){
+
+    }else{
+      if(username.trim() === '' && region === null){
+        setErrorMessage('Please enter a username and select a region.')
+      }else if(username.trim() === ''){
+        setErrorMessage('Please enter a username.')
+      }else if(region === null){
+        setErrorMessage('Please select a region.')
+      }
+    }
+  }
 
   return (
     <>
@@ -13,8 +32,8 @@ export default function SummonerForm() {
         <div className='flex items-center rounded-s-2xl bg-white dark:bg-black px-5 py-3 relative before:absolute before:right-0 before:w-px before:h-8 before:bg-[#80808080]'>
           <MdSearch className='w-7 h-7 mr-5'/>
 
-          <form>
-              <input className='outline-none bg-transparent w-[280px]' placeholder='Enter summoner name or "test"' type="text" name="summonerName" id="summonerName"/>
+          <form onSubmit={handleSubmit}>
+              <input onChange={e => {setUsername(e.target.value)}} className='outline-none bg-transparent w-[280px]' placeholder='Enter summoner name or "test"' type="text" name="summonerName" id="summonerName"/>
           </form>
         </div>
 
@@ -24,12 +43,17 @@ export default function SummonerForm() {
         </button>
       </div>
 
-      <button className='bg-light-blue hover:bg-blue transition text-white rounded-lg px-7 py-2'>Submit</button>
+      <button onClick={handleSubmit} className='bg-light-blue hover:bg-blue transition text-white rounded-lg px-7 py-2'>Submit</button>
 
       <RegionModal 
         isModalActive={isModalActive}
         setIsModalActive={setIsModalActive}
         setRegion={setRegion}
+      />
+
+      <ErrorModal 
+        errorMessage={errorMessage}
+        setErrorMessage={setErrorMessage}
       />
     </>
   )
