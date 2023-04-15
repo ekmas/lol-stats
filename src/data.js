d1523bb4b43e203ec0d49e5e30b55efdae9d0ef2
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-16 01:20:09 +0200

    updated regions obj and added routingvalues

--- src/data.js
+++ src/data.js
@@ -1,6 +1,25 @@
 export const regions = {
-    eu: ['EUNE','EUW'],
+    eu: ['EUNE','EUW', 'RU', 'TR'],
     americas: ['NA', 'LAS', 'LAN', 'BR'],
-    asia: ['RU', 'TR', 'JP', 'KR', 'PH', 'SG', 'TW', 'TH', 'VN'],
-    sea: ['OCE']
+    asia: ['JP', 'KR',],
+    sea: ['PH', 'SG', 'TW', 'TH', 'VN', 'OCE']
+}
+
+export const routingValues = {
+    eune: 'eun1',
+    euw: 'euw1',
+    ru: 'ru',
+    tr: 'tr1',
+    na: 'na1',
+    lan: 'la1',
+    las: 'la2',
+    br: 'br1',
+    jp: 'jp1',
+    kr: 'kr',
+    ph: 'ph2',
+    sg: 'sg2',
+    tw: 'tw2',
+    th: 'th2',
+    vn: 'vn2',
+    oce: 'oc1'
 }
