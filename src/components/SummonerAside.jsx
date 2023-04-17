13d26d66d2d96d54fd32a9264bb1a75f3c896211
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-17 21:47:05 +0200

    added SummonerAside component

---
+++ src/components/SummonerAside.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import RankingSection from './RankingSection'
+
+export default function SummonerAside() {
+  return (
+    <div>
+        <RankingSection />
+    </div>
+  )
+}
--- src/pages/Summoner.jsx
+++ src/pages/Summoner.jsx
@@ -3,13 +3,16 @@ import { useParams } from 'react-router-dom'
 import Header from "../components/Header"
 import useSummonerStore from '../stores/summonerStore'
 import SummonerHeader from '../components/SummonerHeader'
+import SummonerAside from '../components/SummonerAside'
 
 export default function Summoner() {
   let { username, region } = useParams()
 
+  const resetReady = useSummonerStore((state) => state.resetReady)
   const fetch = useSummonerStore((state) => state.fetch)
 
   useEffect(() => {
+    resetReady();
     fetch(username, region)
   }, [])
 
@@ -17,6 +20,11 @@ export default function Summoner() {
     <>
       <Header />
       <SummonerHeader />
+      <div className='pb-[50px] dark:bg-gray bg-white-gray'>
+        <div className='max-w-[1300px] w-auto mx-auto grid grid-cols-[350px_1fr]'>
+          <SummonerAside />
+        </div>
+      </div>
     </>
   )
 }
