a88ff882c50be65940e91d77da4aa5dc6adae7a4
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-21 21:36:33 +0200

    added PlayerLinks and expand button to match

--- src/components/Match.jsx
+++ src/components/Match.jsx
@@ -4,6 +4,8 @@ import MatchMetadata from './MatchMetadata'
 import ChampionRunes from './ChampionRunes'
 import SummonerStats from './SummonerStats'
 import Items from './Items'
+import { MdExpandMore } from 'react-icons/md'
+import PlayerLinks from './PlayerLinks'
 
 export default function Match({ match }) {
 
@@ -14,7 +16,8 @@ export default function Match({ match }) {
   let remake = !seconds ? match.info.gameDuration < 211000 : match.info.gameDuration < 211;
 
   return (
-    <div className={remake ? 'remake' : won ? 'win' : 'loss'}>
+    <div className='flex rounded my-3'>
+      <div className={remake ? 'remake' : won ? 'win' : 'loss'}>
         <MatchMetadata 
           remake={remake}
           won={won}
@@ -32,6 +35,13 @@ export default function Match({ match }) {
           match={match}
           summonerIndex={summonerIndex}
         />
+        <PlayerLinks 
+          match={match}
+        />
+      </div>
+      <button style={{ width: '40px', display: 'flex', justifyContent: 'center', alignItems: 'flex-end', paddingBottom: '8px' }} className={remake ? 'bg-[#9090a280] dark:bg-[#9090a2]' : won ? 'bg-[#5382e880] dark:bg-[#5382e8]' : 'bg-[#E8405780] dark:bg-[#E84057]'}>
+        <MdExpandMore className='fill-white w-6 h-6'/>
+      </button>
     </div>
   )
 }
---
+++ src/components/PlayerLinks.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
+export default function PlayerLinks({ match }) {
+  console.log(match)
+  let players = match.info.participants;
+  let team1 = players.slice(0, 5)
+  let team2 = players.slice(5)
+
+  console.log(team1, team2)
+
+  let { region } = useParams()
+
+  return (
+    <>
+        <div className='grid grid-cols-[1fr_1fr] gap-[10px]'>
+            <div>
+                {team1.map(player => {
+                    return(
+                        <div className='flex items-center'>
+                            <img className='w-[14px] h-[14px] mr-1' key={crypto.randomUUID()} src={`http://ddragon.leagueoflegends.com/cdn/13.8.1/img/champion/${player.championName}.png`} alt="champion" />
+                            <a className='w-20 hover:underline overflow-ellipsis overflow-hidden whitespace-nowrap text-[11px] text-black dark:text-white' href={`/summoner/${region}/${player.summonerName}`}>{player.summonerName}</a>
+                        </div>
+                    )
+                })}
+            </div>
+            <div>
+                {team2.map(player => {
+                    return(
+                        <div className='flex items-center'>
+                            <img className='w-[14px] h-[14px] mr-1' key={crypto.randomUUID()} src={`http://ddragon.leagueoflegends.com/cdn/13.8.1/img/champion/${player.championName}.png`} alt="champion" />
+                            <a className='w-20 hover:underline overflow-ellipsis overflow-hidden whitespace-nowrap text-[11px] text-black dark:text-white' href={`/summoner/${region}/${player.summonerName}`}>{player.summonerName}</a>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    </>
+  )
+}
