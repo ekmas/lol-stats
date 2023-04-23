d2b5527ca4de4d22911b172aa0f1e9b4b248f837
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-23 21:38:54 +0200

    completed MatchPlayer component

--- src/components/MatchPlayer.jsx
+++ src/components/MatchPlayer.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
-import { championsLink, runes, summonerSpell } from '../data'
+import { runes, summonerSpell } from '../data'
+import Items from './Items';
+import { useParams } from 'react-router-dom';
 
-export default function MatchPlayer({ player, match }) {
+export default function MatchPlayer({ player, maxDamage, team }) {
   let rune1 = player.perks.styles[0].selections[0].perk;
   let rune2 = player.perks.styles[1].style;
+  let cs = player.totalMinionsKilled + player.neutralMinionsKilled
+  let damagePercent = (player.totalDamageDealtToChampions / maxDamage) * 100
+
+  let { region } = useParams()
 
   console.log(player)
 
+  /**
+  * @param k kills @param d deaths @param a assists
+  * @returns kda
+  */
+  const calculateKda = (k, d, a) => {
+    let deaths = d === 0 ? 1 : d 
+    // we are setting deaths value to 1 incase it's original value is 0 so we can calculate kda
+    return ((k + a) / deaths).toFixed(2)
+  }
+
   return (
-    <div className='px-4 flex items-center my-3'>
+    <div className='px-4 flex items-center justify-between my-3 text-sm text-black dark:text-white'>
         <div className='flex items-center'>
             <div className="w-[45px] h-[45px] mr-1 relative bg-center bg-cover" style={{ backgroundImage: `url('http://ddragon.leagueoflegends.com/cdn/13.8.1/img/champion/${player.championName}.png')` }}>
                 <div className="absolute left-0 bottom-0 p-0.5 grid place-items-center h-[17px] text-[11px] bg-black text-white">
@@ -26,8 +42,28 @@ export default function MatchPlayer({ player, match }) {
                 </div>
             </div>
         </div>
-        <a className='hover:underline text-sm text-black dark:text-white' href='#'>{player.summonerName}</a>
-
+        <a href={`/summoner/${region}/${player.summonerName}`} className='hover:underline text-sm w-[170px] text-center'>{player.summonerName}</a>
+        <div className='text-center'>
+            <div className='w-[100px] h-2 p-0.5 bg-gray rounded-full'>
+                <div style={{ width: `${damagePercent}%` }} className={team === 'blue' ? 'bg-blue h-full' : 'bg-red h-full'}></div>
+            </div>
+            <p>{player.totalDamageDealtToChampions} DMG</p>
+        </div>
+        <p className='w-[65px] text-center'><span className='font-bold'>{cs}</span> CS</p>
+        <div className="text-center w-[90px]">
+            <div>
+                <b>{player.kills}</b>
+                <span>/</span>
+                <b>{player.deaths}</b>
+                <span>/</span>
+                <b>{player.assists}</b>
+            </div>
+            <p>{calculateKda(player.kills, player.deaths, player.assists)} KDA</p>
+        </div>
+        <Items 
+            player={player}
+            size={'20px'}
+        />
     </div>
   )
 }
