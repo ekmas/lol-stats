aace7976446e6948863bc98e1a215afd4339dcbc
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-23 21:34:40 +0200

    edited item components to make them reusable

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function Item({ item }) {
+export default function Item({ item, size }) {
   return (
     <>
         { item === 0 ? 
-            <div className="item bg-[#00000022] dark:bg-[#ffffff22]"></div> : 
-            <img className='item' src={`https://ddragon.leagueoflegends.com/cdn/13.8.1/img/item/${item}.png`} alt="item" /> 
+            <div style={{ width: `${size}`, height: `${size}` }} className="bg-[#00000022] dark:bg-[#ffffff22]"></div> : 
+            <img style={{ width: `${size}`, height: `${size}` }} src={`https://ddragon.leagueoflegends.com/cdn/13.8.1/img/item/${item}.png`} alt="item" /> 
         }
     </>
   )
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
 import Item from './Item'
 
-export default function Items({ match, summonerIndex }) {
+export default function Items({ match, player, size }) {
   return (
     <div className="grid grid-cols-[1fr_1fr_1fr_1fr] gap-0.5">
         {/* if item id === 0 it means that that item slot is empty so we return item-placeholder */}
         
         <Item 
-            item={match.info.participants[summonerIndex].item0}
+            item={player.item0}
+            size={size}
         />
         <Item 
-            item={match.info.participants[summonerIndex].item1}
+            item={player.item1}
+            size={size}
         />
         <Item 
-            item={match.info.participants[summonerIndex].item2}
+            item={player.item2}
+            size={size}
         />
         <Item 
-            item={match.info.participants[summonerIndex].item3}
+            item={player.item3}
+            size={size}
         />
         <Item 
-            item={match.info.participants[summonerIndex].item4}
+            item={player.item4}
+            size={size}
         />
         <Item 
-            item={match.info.participants[summonerIndex].item5}
+            item={player.item5}
+            size={size}
         />        
 
-        { match.info.participants[summonerIndex].item6 === 0 ? 
-            <div className="item bg-[#00000022] dark:bg-[#ffffff22]"></div> : 
-            <img className='item row-start-1 row-end-2 col-start-4 col-end-5' src={`https://ddragon.leagueoflegends.com/cdn/13.8.1/img/item/${match.info.participants[summonerIndex].item6}.png`} alt="item" /> 
+        { player.item6 === 0 ? 
+            <div style={{ width: `${size}`, height: `${size}` }} className="bg-[#00000022] dark:bg-[#ffffff22]"></div> : 
+            <img style={{ width: `${size}`, height: `${size}` }} className='row-start-1 row-end-2 col-start-4 col-end-5' src={`https://ddragon.leagueoflegends.com/cdn/13.8.1/img/item/${player.item6}.png`} alt="item" /> 
         }
-        <div className="item bg-[#00000022] dark:bg-[#ffffff22]"></div>
+        <div style={{ width: `${size}`, height: `${size}` }} className="bg-[#00000022] dark:bg-[#ffffff22]"></div>
     </div>
   )
 }
