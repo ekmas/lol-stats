b37106c3ba5cace6feb5522b99faf878990dd3a0
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-25 19:43:42 +0200

    added Leaderboard page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import { ThemeContext } from "./contexts/ThemeContext";
 import Home from "./pages/Home"
 import Summoner from "./pages/Summoner";
+import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   const [theme, setTheme] = useState(null);
@@ -29,6 +30,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/summoner/:region/:username" element={<Summoner />}/>
+        <Route path="/leaderboard" element={<Leaderboard />}/>
       </Routes>
     </ThemeContext.Provider>
   )
