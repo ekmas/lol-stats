428d393ce44d6c74a1735466a1a9a759ae645b7d
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-25 19:31:53 +0200

    added a link to summoner and changed styles

--- src/components/Teammate.jsx
+++ src/components/Teammate.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Teammate({ stats }) {
 
   let winrate = (((stats.wins / (stats.wins + stats.losses)) * 100).toFixed(0))
+
+  let { region } = useParams()
 
   return (
     <div className='my-3 flex justify-between'>
@@ -10,11 +13,11 @@ export default function Teammate({ stats }) {
             <div className='w-10 h-10 mr-3 bg-center bg-cover' style={{ backgroundImage: `url(http://ddragon.leagueoflegends.com/cdn/13.8.1/img/profileicon/${stats.pfp}.png)`}}></div>
 
             <div className='h-10 flex items-center'>
-                <h4 className='text-sm font-bold text-black dark:text-white'>{stats.name}</h4>
+                <a href={`/summoner/${region}/${stats.name}`} className='hover:underline text-sm font-bold text-black dark:text-white'>{stats.name}</a>
             </div>
         </div>
         <div className='flex items-center'>
-            <p className='text-sm mr-3 font-medium' style={{ color: winrate > 50 ? 'green' : winrate === '50' ? 'gray' : 'red' }}>{winrate}%</p>
+            <p className='text-sm mr-3 font-medium' style={{ color: winrate > 50 ? '#E84057' : 'gray' }}>{winrate}%</p>
 
             <p className='text-sm text-black dark:text-white'>{stats.wins}W/{stats.losses}L</p>
         </div>
