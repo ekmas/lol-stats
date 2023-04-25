46d07124c7395b73e08fb662849fd61b554b8a37
Samuel Breznjak <samuelbreznjak35@gmail.com>
2023-04-25 17:53:04 +0200

    removed challenges

--- src/components/SummonerMain.jsx
+++ src/components/SummonerMain.jsx
@@ -12,17 +12,12 @@ export default function SummonerMain() {
     <>
       {ready ?
         <div className='bg-white dark:bg-black px-4 py-3 rounded-lg'>
-            <div className='grid grid-cols-[1fr_1fr_1fr] text-black dark:text-white'>
+            <div className='grid grid-cols-[1fr_1fr] text-black dark:text-white'>
                 <button onClick={() => {setActiveSection('matches')}} className={activeSection === 'matches' ? 'relative before:absolute before:w-full before:bottom-0 before:left-0 before:h-px dark:before:bg-white before:bg-black py-3' : 'py-3'}>Matches</button>
                 <button onClick={() => {setActiveSection('mastery')}} className={activeSection === 'mastery' ? 'relative before:absolute before:w-full before:bottom-0 before:left-0 before:h-px dark:before:bg-white before:bg-black py-3' : 'py-3'}>Mastery</button>
-                <button onClick={() => {setActiveSection('challenges')}} className={activeSection === 'challenges' ? 'relative before:absolute before:w-full before:bottom-0 before:left-0 before:h-px dark:before:bg-white before:bg-black py-3' : 'py-3'}>Challenges</button>
             </div>
             <div className='pt-2'>
-                {
-                    activeSection === 'matches' ? <Matches /> : 
-                    activeSection === 'mastery' ? <Mastery /> : 
-                    <p>challenges</p>
-                }
+                {activeSection === 'matches' ? <Matches /> : <Mastery />}
             </div>
         </div>
         :
